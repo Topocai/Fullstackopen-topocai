cce7a48c431c6c4f1baec66cb62c2f48002a2818
topocai <leonjoserp@gmail.com>
2024-07-30 14:20:07 -0300

    Part7 to 7.6

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react"
 
+import { useField } from '../hooks'
+
 import { useNotificationDispatch, setNotification } from "../hooks/notification"
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const notifyDispatch = useNotificationDispatch()
 
@@ -21,33 +23,42 @@ const CreateNew = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault()
     addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    e.target.value = ''
+    content.onChange(e)
+    author.onChange(e)
+    info.onChange(e)
   }
   
   return (
     <article>
       <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <fieldset>
           <legend>Create new anecdote</legend>
           <label>
             content
-            <input name='content' value={content} onChange={(e) => setContent(e.target.value)} required/>
+            <input name='content' {...content} required/>
           </label>
           <label>
             author
-            <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} required/>
+            <input name='author' {...author} required/>
           </label>
           <label>
             url for more info
-            <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} required/>
+            <input name='info' {...info} required/>
           </label>
         </fieldset>
         <button>create</button>
+        <button onClick={(e) => handleReset(e)}>reset</button>
       </form>
     </article>
   )
