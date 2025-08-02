03c439dbceb18145dfaf51a88253aa1b5fbca980
topocai <topocaidev@gmail.com>
2025-08-01 23:52:12 -0300

    8.5

--- part8/definitions/books.js
+++ part8/definitions/books.js
@@ -9,11 +9,28 @@ const definitions = `
         id: ID!
     }
 `;
-const queries = `bookCount: Int!\nallBooks: [Book!]!`;
+const queries = `bookCount: Int!\nallBooks(author: String, genre: String): [Book!]!`;
 
 const queryResolver = {
   bookCount: () => books.length,
-  allBooks: () => books,
+  allBooks: (root, args) => {
+    if (!args) {
+      return books;
+    }
+
+    let filtered = books;
+    if (args.author) {
+      filtered = filtered.filter(
+        (book) => book.author.toLowerCase() === args.author.toLowerCase()
+      );
+    }
+
+    if (args.genre) {
+      filtered = filtered.filter((book) => book.genres.includes(args.genre));
+    }
+
+    return filtered;
+  },
 };
 
 module.exports = { definitions, queries, queryResolver };
